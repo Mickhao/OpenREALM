61383140486db26c838c518d49de9298f7ba5274
HaoN <52821409+Mickhao@users.noreply.github.com>
2023-08-26 19:18:20 +0800

    c

--- modules/realm_core/src/settings_base.cpp
+++ modules/realm_core/src/settings_base.cpp
@@ -5,12 +5,15 @@
 using namespace realm;
 
 // PUBLIC
+//为了管理一组设置参数
 
+//重载:当你使用 settings[key] 时，它实际上调用了 get(key) 函数来获取参数的值并返回
 SettingsBase::Variant SettingsBase::operator[](const std::string &key) const
 {
   return get(key);
 }
 
+//用于从设置中获取给定键 key 对应的参数值
 SettingsBase::Variant SettingsBase::get(const std::string &key) const
 {
   auto it = m_parameters.find(key);
@@ -20,33 +23,42 @@ SettingsBase::Variant SettingsBase::get(const std::string &key) const
     throw(std::out_of_range("Error: Parameter with name '" + key + "' could not be found in settings."));
 }
 
+//设置键为 key 的参数值为整数类型 val
 void SettingsBase::set(const std::string &key, int val)
 {
   m_parameters[key]->m_int_container.value = val;
 }
 
+//设置键为 key 的参数值为浮点数类型 val
 void SettingsBase::set(const std::string &key, double val)
 {
   m_parameters[key]->m_double_container.value = val;
 }
 
+//设置键为 key 的参数值为字符串类型 val
 void SettingsBase::set(const std::string &key, const std::string &val)
 {
   m_parameters[key]->m_string_container.value = val;
 }
 
+//设置键为 key 的参数值为 OpenCV 的矩阵（cv::Mat）类型 val
 void SettingsBase::set(const std::string &key, const cv::Mat &val)
 {
   m_parameters[key]->m_mat_container.value = val;
 }
 
+//用于从文件中加载参数设置
 void SettingsBase::loadFromFile(const std::string &filepath)
 {
+  //打开指定路径的配置文件，创建 cv::FileStorage 对象 fs 以进行读取
   cv::FileStorage fs(filepath, cv::FileStorage::READ);
+  //检查文件是否成功打开
   if (fs.isOpened())
   {
+    //遍历每个参数对象 param
     for (auto &param : m_parameters)
     {
+      //根据参数对象的类型,使用 fs 对应的键值将文件中的值读取到参数对象的容器中
       if (param.second->m_type == SettingsBase::Variant::VariantType::INT)    fs[param.first] >> param.second->m_int_container.value;
       if (param.second->m_type == SettingsBase::Variant::VariantType::DOUBLE) fs[param.first] >> param.second->m_double_container.value;
       if (param.second->m_type == SettingsBase::Variant::VariantType::STRING) fs[param.first] >> param.second->m_string_container.value;
@@ -57,6 +69,7 @@ void SettingsBase::loadFromFile(const std::string &filepath)
     throw std::out_of_range("Error. Could not load settings from file: " + filepath);
 }
 
+//用于检查给定键是否存在于参数集合中
 bool SettingsBase::has(const std::string &key) const
 {
   auto it = m_parameters.find(key);
@@ -66,6 +79,7 @@ bool SettingsBase::has(const std::string &key) const
     return false;
 }
 
+//用于将参数集合的内容打印到标准输出
 void SettingsBase::print()
 {
   std::cout.precision(6);
@@ -80,21 +94,25 @@ void SettingsBase::print()
 
 // PROTECTED
 
+//接受一个键和一个整数类型的参数，并将参数的值添加到参数集合中
 void SettingsBase::add(const std::string &key, const Parameter_t<int> &param)
 {
   m_parameters[key].reset(new Variant(param));
 }
 
+//接受一个键和一个双精度浮点数类型的参数，并将参数的值添加到参数集合中
 void SettingsBase::add(const std::string &key, const Parameter_t<double> &param)
 {
   m_parameters[key].reset(new Variant(param));
 }
 
+//接受一个键和一个字符串类型的参数，并将参数的值添加到参数集合中
 void SettingsBase::add(const std::string &key, const Parameter_t<std::string> &param)
 {
   m_parameters[key].reset(new Variant(param));
 }
 
+//接受一个键和一个 OpenCV cv::Mat 类型的参数，并将参数的值添加到参数集合中
 void SettingsBase::add(const std::string &key, const Parameter_t<cv::Mat> &param)
 {
   m_parameters[key].reset(new Variant(param));
--- modules/realm_core/src/timer.cpp
+++ modules/realm_core/src/timer.cpp
@@ -4,18 +4,21 @@
 
 using namespace realm;
 
+//创建一个定时器对象
 Timer::Timer(const std::chrono::milliseconds &period, const std::function<void()> &func)
-    : m_period(period),
-      m_func(func),
-      m_in_flight(true)
+    : m_period(period),//传入的时间间隔
+      m_func(func), //传入的函数
+      m_in_flight(true) //表示定时器处于活动状态
 {
   // Lambda implementation for threading
-  m_thread = std::thread([this]
+  //启动一个新线程
+  m_thread = std::thread([this] 
   {
     while (m_in_flight)
     {
+      //等待指定的时间间隔
       this->interruptable_wait_for(m_period);
-      if (m_in_flight)
+      if (m_in_flight) //如果为真,则调用成员函数 
       {
         m_func();
       }
@@ -26,14 +29,15 @@ Timer::Timer(const std::chrono::milliseconds &period, const std::function<void()
 Timer::~Timer()
 {
   {
+    //创建一个 std::lock_guard 对象 l，使用互斥锁 m_stop_mtx 对象进行上锁
     std::lock_guard<std::mutex> l(m_stop_mtx);
-      m_in_flight = false;
+      m_in_flight = false;//表示定时器不再处于活动状态
   }
-  m_cond.notify_one();
-  m_thread.join();
+  m_cond.notify_one();//发送一个通知，以唤醒可能在等待中的线程
+  m_thread.join();//等待定时器所在的线程结束
 }
 
-
+//获取当前时间的秒数，精确到整秒
 long Timer::getCurrentTimeSeconds()
 {
   using namespace std::chrono;
@@ -41,6 +45,7 @@ long Timer::getCurrentTimeSeconds()
   return s.count();
 }
 
+//获取当前时间的毫秒数，精确到毫秒
 long Timer::getCurrentTimeMilliseconds()
 {
   using namespace std::chrono;
@@ -48,6 +53,7 @@ long Timer::getCurrentTimeMilliseconds()
   return ms.count();
 }
 
+//获取当前时间的微秒数，精确到微秒
 long Timer::getCurrentTimeMicroseconds()
 {
   using namespace std::chrono;
@@ -55,6 +61,7 @@ long Timer::getCurrentTimeMicroseconds()
   return ms.count();
 }
 
+//获取当前时间的纳秒数，精确到纳秒
 long Timer::getCurrentTimeNanoseconds()
 {
   using namespace std::chrono;
